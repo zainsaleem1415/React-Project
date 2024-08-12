20f4b95d770d64a784a463ce5978ac13248569ae
Zain Saleem <zainsaleem1415@gmail.com>
2024-08-12 21:41:38 +0500

    react practice

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './component5/Navbar';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
- 
-  
-  const [count,setcount] = useState(0)
-  const [first,setfirst] = useState(0)
-  const [color,setcolor] = useState(0)
-  
 
 
-useEffect(()=>{
-  alert("Count was Changed")
-  setcolor(color+1)
-},[count] )
+  const [count, setcount] = useState(0)
+  const [first, setfirst] = useState(0)
+  const [color, setcolor] = useState(0)
+
+  const a = useRef(0)
+  useEffect(() => {
+    a.current = a.current + 1
+    console.log("Rerendering and the value of a is ${a.current} ")
+  })
+
+
+  useEffect(() => {
+    alert("Count was Changed")
+    setcolor(color + 1)
+  }, [count])
 
   return (
     <>
-     { <div className='counter'>
-     
-    <button onClick={ () => { setcount(count+1) } } >Count is {count}</button>
-    
-    </div> }
-    <Navbar color={"blue " + " navy  "+"   " +color } />
+      {<div className='counter'>
+
+        <button onClick={() => { setcount(count + 1) }} >Count is {count}</button>
+
+      </div>}
+      <Navbar color={"blue " + " navy  " + "   " + color} />
     </>
-    
+
   );
 }
 
